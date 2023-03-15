38ab115d681305924727d1f21c4ae74bcf24debd
MSRbuddy <meghanadasari222@gmail.com>
2023-03-15 17:26:06 +0530

    Employee Payroll Problem using REST-Sharpgit add Program.csgit add Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpPayroll_REST_API
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee-Paroll Services using REST-API!");
+        }
+    }
+}
